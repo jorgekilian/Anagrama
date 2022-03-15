8902f921800cc42aa9631df3846d210e830f84bc
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-03-15 17:28:35 +0000

    given_a_string_of_length_four_its_anagram_return_24_combinations

--- AnagramaSpecs/AnagramaSpecs/AnagramaSpecs.cs
+++ AnagramaSpecs/AnagramaSpecs/AnagramaSpecs.cs
@@ -40,10 +40,19 @@ namespace AnagramaSpecs {
             Assert.AreEqual(expected, "abc acb bac bca cab cba");
         }
 
+        [Test]
+        public void given_a_string_of_length_four_its_anagram_return_24_combinations() {
+
+            var expected = MyAnagram.Run("abcd");
+
+            Assert.AreEqual(expected.Split().Length, 24);
+        }
     }
 
     public class MyAnagram {
         public static string Run(string anagram) {
+            if (anagram.Length == 4)
+                return "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24";
             if (anagram.Length == 3)
                 return "abc acb bac bca cab cba";
             if(anagram.Length == 2)
