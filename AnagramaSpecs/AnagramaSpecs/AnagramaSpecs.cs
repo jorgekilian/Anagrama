f19e49fd4b45d73dba4f9ca7e32fe27c62e2dca7
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-03-15 17:15:35 +0000

    given_a_string_of_length_two_its_anagram_is_the_same_string_and_the_inverse

--- AnagramaSpecs/AnagramaSpecs/AnagramaSpecs.cs
+++ AnagramaSpecs/AnagramaSpecs/AnagramaSpecs.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace AnagramaSpecs {
@@ -23,12 +24,22 @@ namespace AnagramaSpecs {
             Assert.AreEqual(expected, "a");
         }
 
+        [Test]
+        public void given_a_string_of_length_two_its_anagram_is_the_same_string_and_the_inverse() {
+
+            var expected = MyAnagram.Run("ab");
+
+            Assert.AreEqual(expected, "ab ba");
+        }
 
     }
 
     public class MyAnagram {
         public static string Run(string anagram) {
-            return anagram;
+            if (anagram.Length > 1)
+                return $"{anagram} {new string(anagram.Reverse().ToArray())}";
+            else
+                return anagram;
         }
     }
 }
