47f6fbe2ab8fc4f5a5b73f59a650d82ed4eb2519
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-03-15 16:55:02 +0000

    given_an_empty_string_its_anagram_is_an_empty_string

---
+++ AnagramaSpecs/AnagramaSpecs/AnagramaSpecs.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace AnagramaSpecs {
+    public class AnagramaSpecs {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void given_an_empty_string_its_anagram_is_an_empty_string() {
+
+            var expected = MyAnagram.Run(string.Empty);
+
+            Assert.Pass(expected, string.Empty);
+        }
+    }
+
+    public class MyAnagram {
+        public static string Run(string empty) {
+            return string.Empty;
+        }
+    }
+}
