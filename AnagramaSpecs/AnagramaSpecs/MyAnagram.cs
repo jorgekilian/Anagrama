ceb5fe85fd3b2070573d3ee8c0c8771ab78bbd15
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-03-22 16:42:23 +0000

    refactor

--- AnagramaSpecs/AnagramaSpecs/MyAnagram.cs
+++ AnagramaSpecs/AnagramaSpecs/MyAnagram.cs
@@ -7,30 +7,24 @@ namespace AnagramaSpecs {
             if (anagram.Length >= 3) {
                 var combinations = string.Empty;
                 var intPtr = GetCombinations(anagram.Length);
-                string anagramTemp;
+                var anagramTemp = anagram;
                 for (var i = 0; i < intPtr; i++) {
                     if (i == 1) {
                         anagramTemp = $"{anagram.Substring(0, 1)}{anagram.Substring(2, 1)}{anagram.Substring(1, 1)}{anagram.Substring(3)}";
-                        combinations = $"{combinations} {anagramTemp}";
                     }
                     else if (i == 2) {
                         anagramTemp = $"{anagram.Substring(1, 1)}{anagram.Substring(0, 1)}{anagram.Substring(2)}";
-                        combinations = $"{combinations} {anagramTemp}";
                     }
                     else if (i == 3) {
                         anagramTemp = $"{anagram.Substring(1, 1)}{anagram.Substring(2, 1)}{anagram.Substring(0, 1)}{anagram.Substring(3)}";
-                        combinations = $"{combinations} {anagramTemp}";
                     }
                     else if (i == 4) {
                         anagramTemp = $"{anagram.Substring(2, 1)}{anagram.Substring(0, 1)}{anagram.Substring(1, 1)}{anagram.Substring(3)}";
-                        combinations = $"{combinations} {anagramTemp}";
                     }
                     else if (i == 5) {
                         anagramTemp = $"{anagram.Substring(2, 1)}{anagram.Substring(1, 1)}{anagram.Substring(0, 1)}{anagram.Substring(3)}";
-                        combinations = $"{combinations} {anagramTemp}";
                     }
-                    else
-                        combinations = $"{combinations} {anagram}";
+                    combinations = $"{combinations} {anagramTemp}";
                 }
                 return combinations.Trim();
             }
